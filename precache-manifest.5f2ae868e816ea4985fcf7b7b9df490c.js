3e6126ecb36eaa0c76d476fe2df51a0ed52da02b
Roman <romanroshchuk@yahoo.com>
2019-04-11 16:16:39 -0700

    Updates

---
+++ precache-manifest.5f2ae868e816ea4985fcf7b7b9df490c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0387bc89b0e3bf0e9623",
+    "url": "/R0M2017/my-app.git/static/css/main.1788bfdb.chunk.css"
+  },
+  {
+    "revision": "0387bc89b0e3bf0e9623",
+    "url": "/R0M2017/my-app.git/static/js/main.759666b1.chunk.js"
+  },
+  {
+    "revision": "6b47f3fe3bb446dcac50",
+    "url": "/R0M2017/my-app.git/static/js/runtime~main.82838c6c.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/R0M2017/my-app.git/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "8e8b280840f3c0cf8eb093e507317eda",
+    "url": "/R0M2017/my-app.git/index.html"
+  }
+];
